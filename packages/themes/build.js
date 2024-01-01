c2f9b038c0e0fe90af5147734e1f5a52a90abc7d
dev1week <rondo2860@gmail.com>
2024-01-01 11:23:09 +0900

    chroe: cjs, esm 빌드 전부 제공(#1)

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,5 +1,6 @@
 import esbuild from "esbuild";
 
+//es 모듈 제공하기
 esbuild.build({
   entryPoints: ["src/index.js"],
   bundle: true,
@@ -7,4 +8,17 @@ esbuild.build({
   sourcemap: true,
   outdir: "dist",
   format: "esm",
+});
+// cjs 빌드 제공하기
+esbuild.build({
+  entryPoints: ["src/index.js"],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  outdir: "dist",
+  format: "cjs",
+  //확장자를 전부 .js에서 .cjs로 바꾸기
+  outExtension: {
+    ".js": ".cjs",
+  },
 });
