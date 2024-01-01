41dd1b9aee283a4917512094e12cca91533a39ed
dev1week <rondo2860@gmail.com>
2024-01-01 11:26:40 +0900

    refactor: 빌드 파일 병렬화 및 공통 설정 추출(#1)

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,24 +1,29 @@
 import esbuild from "esbuild";
+//공통 속성 뽑기
+const baseConfig = {
+  entryPoints: ["src/index.js"],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  outdir: "dist",
+};
 
-//es 모듈 제공하기
-esbuild.build({
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outdir: "dist",
-  format: "esm",
-});
-// cjs 빌드 제공하기
-esbuild.build({
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outdir: "dist",
-  format: "cjs",
-  //확장자를 전부 .js에서 .cjs로 바꾸기
-  outExtension: {
-    ".js": ".cjs",
-  },
+//병렬화
+Promise.all([
+  //es 모듈 제공하기
+  esbuild.build({
+    format: "esm",
+  }),
+  // cjs 빌드 제공하기
+  esbuild.build({
+    ...baseConfig,
+    format: "cjs",
+    //확장자를 전부 .js에서 .cjs로 바꾸기
+    outExtension: {
+      ".js": ".cjs",
+    },
+  }),
+]).catch(() => {
+  console.error(...(data + "Build Failed"));
+  ProcessingInstruction.exit(1);
 });
